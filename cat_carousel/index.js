791c34a8644de9de768c741cfa4e656dc49b2b45
Nicci Williams <nicci@nicciwill.com>
2017-11-02 12:52:12 -0400

    Cat carousel exercise

--- cat_carousel/index.js
+++ cat_carousel/index.js
@@ -1,1 +1,23 @@
-// Your code goes here.
+/*function catCarousel(){
+	$('.thumbnail').on('click', event => {
+		console.log(event);
+		alert("You clicked me");
+	});
+
+}*/
+
+function catCarousel(){
+  $('.thumbnail').on('click', function(e){
+  	console.log(e);
+  	console.log(this);
+    let source = $(this).find('img').attr('src');
+    let altText = $(this).find('img').attr('alt');
+   
+    $('.hero img').attr('src', source);
+    $('.hero img').attr('alt', altText);
+    
+    
+  })
+}
+
+$(catCarousel);
